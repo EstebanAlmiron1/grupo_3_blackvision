252851255231f891a7f412fbeccc4024340e83c8
JCenturionDiaz <jonathancenturiondiaz@gmail.com>
2023-07-15 16:15:07 -0300

    Se creó carrito de compras

--- app.js
+++ app.js
@@ -11,3 +11,6 @@ app.listen(3000,()=>{
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
 })
+app.get("/carrito",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/carrito.html"))
+})

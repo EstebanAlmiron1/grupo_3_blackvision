3d62f3fbe8b63d27f6ab002fd9169049bb45cc81
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-07-20 19:08:55 -0300

    cree login y carrito .css

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
-app.use(express.static("public"))
 
 app.listen(3000,()=>{
     console.log("BlackVision activado")
 })
+app.use(express.static("public"))
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))

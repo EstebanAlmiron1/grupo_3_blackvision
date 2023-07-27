22139c9946a6ca25afaee98c55268f6d0f21d44a
Esteban Almiron <estebanalmiron1@gmail.com>
2023-07-27 18:33:02 -0300

    Cree productList y cambie rutas

--- app.js
+++ app.js
@@ -30,3 +30,7 @@ app.get("/productDetail",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/register.html"))
 })
+
+app.get("/productList",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/productList.html"))
+})

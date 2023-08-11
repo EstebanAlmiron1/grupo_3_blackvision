06ff6aa157e75fa3e299538aeecc8953422b8155
juancruzvaschetto <vaschettojuancruz0@gmail.com>
2023-08-10 22:36:16 -0300

    creamos controllers y routers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+
+const mainRouter=require("./routers/mainRouter")
+const productRouter=require("./routers/productRouter")
+const userRouter=require("./routers/userRouter")
+
+
+app.use(express.static("public"))
+
 app.listen(3000,()=>{
     console.log("BlackVision activado")
 })
 
-app.use(express.static("public"))
+app.use("/",mainRouter)
+//app.use("/product",productRouter)
+//app.use("/user",userRouter)
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/index.html"))
-})
+//app.set("views",path.join(__dirname,"views"))//
+//app.set("view engine","ejs")//
+
+
+
 
 app.get("/productCart",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/productCart.html"))
---
+++ src/controllers/mainController.js
@@ -0,0 +1,16 @@
+const path=require('path')
+
+const controller ={
+    home:(req,res)=>{        
+        return res.sendFile(path.join(__dirname,"../views/index.html"))
+    }
+}
+
+
+
+
+
+
+
+
+module.exports = controller

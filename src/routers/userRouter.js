9e7cf75728f520a710b3a089913f898137e14224
JCenturionDiaz <jonathancenturiondiaz@gmail.com>
2023-09-06 22:46:06 -0300

    Se crea vista users y se encripta contraseña

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,9 +9,8 @@ router.get("/login",controller.login)
 router.get("/register",controller.register)
 router.post("/register",uploadFile.single('imageus'),controller.registerProcess)
 router.get("/cart",controller.cart)
-router.get("/:id",controller.profile)
 router.get("/userResults", controller.search)
-
-
+router.get("/list", controller.list)
+router.get("/:id",controller.profile)
 
 module.exports=router

2d37b2b4ec7c128205f310d21331cd7342994f65
fiamle <fiammaledesma2000@gmail.com>
2023-10-08 20:56:40 -0300

    cambios en config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "black_vision",
-    "host": "127.0.0.1",
+    "username": "329910",
+    "password": "black-vision" ,
+    "database": "blackvision_db",
+    "host": "mysql-blackvision.alwaysdata.net",   // https://phpmyadmin.alwaysdata.com/phpmyadmin/index.php?route=/
     "dialect": "mysql"
   },
   "test": {

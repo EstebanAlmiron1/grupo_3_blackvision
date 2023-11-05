86e932feb0aafc93ef5dcb6a8137ef9feb3461e8
EstebanAlmiron1 <estebanalmiron1@gmail.com>
2023-11-05 18:06:27 -0300

    Validaciones del front

--- src/middlewares/validatorCreateProducts.js
+++ src/middlewares/validatorCreateProducts.js
@@ -1,7 +1,7 @@
 const {check} = require('express-validator')
 const validatorCreate = [
-    check('name').notEmpty().withMessage('*El nombre es un campo obligatorio').bail().isLength({min:5}).withMessage('El nombre debe tener minimo 5 caracteres') ,
-    check('description').notEmpty().withMessage('*La descripción es un campo obligatorio').bail().isLength({min:20}).withMessage('La descripcion debe tener minimo 20 caracteres') ,
+    check('name').notEmpty().withMessage('*El nombre es un campo obligatorio').bail().isLength({min:5}).withMessage('*El nombre debe tener minimo 5 caracteres') ,
+    check('description').notEmpty().withMessage('*La descripción es un campo obligatorio').bail().isLength({min:20}).withMessage('*La descripcion debe tener minimo 20 caracteres') ,
     check('color').notEmpty().withMessage('*El color debe ser obligatorio') ,
     check('size').notEmpty().withMessage('*El talle es un campo obligatorio') ,
     check('category').notEmpty().withMessage('*La categoria es obligatoria'),
